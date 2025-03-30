5b2c36a13a6bb76af1ff6fe3cfab2a9735455536
DaHuangQwQ <88641334+DaHuangQwQ@users.noreply.github.com>
2025-03-30 15:24:36 +0800

    env

--- .script/setup.sh
+++ .script/setup.sh
@@ -7,6 +7,14 @@ pip install pre-commit
 echo "正在同步包"
 uv sync
 
+# 检查 .env 文件是否存在
+if [ ! -f ./.env ]; then
+    echo "设置环境变量"
+    cp .script/.template_env ./.env
+else
+    echo ".env 文件已存在，跳过设置"
+fi
+
 echo "正在部署自动化测试"
-export PRE_COMMIT_CONFIG=.script/.pre-commit-config.yaml
+#export PRE_COMMIT_CONFIG=.script/.pre-commit-config.yaml
 pre-commit install
